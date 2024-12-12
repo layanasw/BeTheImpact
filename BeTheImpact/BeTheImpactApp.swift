6bfa4f6b4f77ca9aa724c5543b8252aa31568257
layanasw <lyan.wasaidi2@gmail.com>
2024-12-12 15:37:00 +0300

    Initial Commit

---
+++ BeTheImpact/BeTheImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeTheImpactApp.swift
+//  BeTheImpact
+//
+//  Created by layan alwasaidi on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BeTheImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
